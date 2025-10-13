49501eb93776a8a9e2dbae883b5d538a1d6afe19
MichaelneGyn <michaeldouglasqueiroz@gmail.com>
2025-10-13 11:28:03 -0300

    fix: switch from Prisma Accelerate to direct Supabase connection with transaction pooler

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: ['query'],
-  }).$extends(withAccelerate())
+  })
+  // .$extends(withAccelerate())
 }
 
 declare const globalThis: {
